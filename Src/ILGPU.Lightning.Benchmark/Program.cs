499bcea7f6c1e143b7d7d2ec018a14f249147455
Mikhail Khalizev <exomicky53@gmail.com>
2019-06-19 11:26:02 +0300

    Add BaseBenchmark.

--- Src/ILGPU.Lightning.Benchmark/Program.cs
+++ Src/ILGPU.Lightning.Benchmark/Program.cs
@@ -1,41 +1,19 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Security.Cryptography;
-using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Running;
 
 namespace ILGPU.Lightning.Benchmark
 {
-    public class Md5VsSha256
-    {
-        private const int N = 10000;
-        private readonly byte[] data;
-
-        private readonly SHA256 sha256 = SHA256.Create();
-        private readonly MD5 md5 = MD5.Create();
-
-        public Md5VsSha256()
-        {
-            data = new byte[N];
-            new Random(42).NextBytes(data);
-        }
-
-        [Benchmark]
-        public byte[] Sha256() => sha256.ComputeHash(data);
-
-        [Benchmark]
-        public byte[] Md5() => md5.ComputeHash(data);
-    }
-
     public class Program
     {
         public static void Main(string[] args)
         {
             GpuHostEnvironmentInfo.SetupToHostEnvironmentInfo();
 
-            var summary = BenchmarkRunner.Run<Md5VsSha256>();
-            Console.ReadLine();
+            // See https://benchmarkdotnet.org/articles/guides/console-args.html
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig()); // TODO Remove DebugInProcessConfig!
         }
     }
 }
---
+++ Src/ILGPU.Lightning.Benchmark/ReduceBenchmark.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using BenchmarkDotNet.Attributes;
+using ILGPU.Runtime;
+
+namespace ILGPU.Lightning.Benchmark
+{
+    public abstract class BaseBenchmark
+    {
+        [ParamsSource(nameof(ValuesForAcceleratorId))]
+        public AcceleratorId AcceleratorId { get; set; }
+
+        public IEnumerable<AcceleratorId> ValuesForAcceleratorId => GpuHostEnvironmentInfo.Instance.GpuDeviceNames.Keys;
+
+
+        [ParamsSource(nameof(ValuesForBaseLength))]
+        public int BaseLength { get; set; }
+
+        public IEnumerable<int> ValuesForBaseLength => Enumerable.Range(0, 31).Select(x => 16 << x).TakeWhile(x => x <= 10_000_000);
+
+
+        [GlobalSetup]
+        public virtual void GlobalSetup()
+        {
+
+        }
+
+        [GlobalCleanup]
+        public virtual void GlobalCleanup()
+        {
+
+        }
+    }
+
+    public class ReduceBenchmark : BaseBenchmark
+    {
+        public override void GlobalSetup()
+        {
+            base.GlobalSetup();
+        }
+
+        public override void GlobalCleanup()
+        {
+            base.GlobalCleanup();
+        }
+
+
+        [Benchmark]
+        public void Sha256()
+        {
+            Thread.Sleep(Math.Min(100, BaseLength));
+            //return sha256.ComputeHash(data);
+        }
+    }
+}
